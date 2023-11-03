ba465a4030b77747680285ad568a92740403de41
Igor Konoval <kip_y@sfa.org.ua>
2023-11-03 20:31:43 +0200

    edit error

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -12,9 +12,9 @@ import {
   ProfileQuantity,
 } from './Profile.styled';
 
-export const Profile = ({
-  user: { username, tag, location, avatar, stats },
-}) => {
+export const Profile = ({ user }) => {
+  const { username, tag, location, avatar, stats } = user;
+
   return (
     <ProfileContainer>
       <ProfileDescription>
@@ -42,17 +42,17 @@ export const Profile = ({
 };
 
 Profile.propTypes = {
-  username: PropTypes.string.isRequired,
-  tag: PropTypes.string.isRequired,
-  location: PropTypes.string.isRequired,
-  avatar: PropTypes.string.isRequired,
-  stats: PropTypes.shape(
-    {
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.shape({
       followers: PropTypes.number.isRequired,
       views: PropTypes.number.isRequired,
       likes: PropTypes.number.isRequired,
-    }.isRequired
-  ).isRequired,
+    }).isRequired,
+  }).isRequired,
 };
 
 export default Profile;
